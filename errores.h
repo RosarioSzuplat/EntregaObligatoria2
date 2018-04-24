b4fe5c47bf13d19502c3f91287ca3ffe0c95afae
Rosario Szuplat <szuplat.rosario@gmail.com>
2018-04-23 22:23:33 -0300

    definiciones de mensajes de error utilizados en TODOS los ejericios.

---
+++ errores.h
@@ -0,0 +1,15 @@
+#ifndef ERROR_H
+#define ERROR_H
+
+#define MSJ_ERROR "Se ha producido un error"
+#define MSJ_INVALIDO "El numero ingresado no pertenece al intervalo designado."
+#define MSJ_NUMERO_INVALIDO "El caracter ingresado no es un numero entero."
+#define MSJ_NEGATIVO "El numero ingresado no puede ser negativo."
+#define MSJ_CARACTER_INVALIDO "El caracter ingresado no es numero."
+#define MSJ_NO_ESCALA "No se introducio una escala existente."
+#define MSJ_NO_LOGICO "No se han introducido valores coherentes."
+#define MSJ_RESPUESTA_INVALIDA "La respuesta ingresada no es valida."
+#define MSJ_CUADRADA "La matriz debe ser cuadrada para poder calcular el determinante."
+#define MSJ_DIMENSION "Error de dimension."
+
+#endif
