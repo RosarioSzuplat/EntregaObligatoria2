2055fba4e74edc38eab5b8c6d1db7ffed222b1c6
RosarioSzuplat <37803380+RosarioSzuplat@users.noreply.github.com>
2018-04-30 19:35:09 -0300

    se corrigio el ejercicio
    Sin embargo no se como usar el puntero ptr

--- punteros.c
+++ punteros.c
@@ -1,10 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 int main (void) {
 	int matriz[3][4] = {{ 1, 2, 3, 4}, { 5, 6, 7, 8}, { 9,10,11,12}};
-	int *ptr;
-	int (*ptr2vector)[4]; /* ptr2vector es un  puntero a un arreglo de 4 enteros */
+	int * ptr; /*no lo uso*/
+	int (*ptr2vector)[4]; /*puntero a un arreglo de 4 enteros, entonces apuntara a cada fila de la matriz*/
 	int fila, col;
-	for (fila = 0; fila < 3; fila++){
-			for (col = 0; col < columnas; col++)
-				printf("%d  ", *(*(matriz + fila) + col)); /* uso: matriz [fila] [columna] equivalente a *(*(matriz + fila) +j) */
-			printf("\n"); /* pasa al renglon siguiente cada vez que se termina de escribir una fila */
-		}
+	ptr2vector = matriz;
+	for (fila = 0; fila < 3; fila ++) {
+		for (col = 0; col < 4; col ++) 
+			printf("%d  ", *(*(ptr2vector + fila) + col));/*matriz [i] [j]*/
+		printf("\n");
+	}
+	return EXIT_SUCCESS;
+}
